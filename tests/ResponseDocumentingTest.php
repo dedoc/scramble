13f28d02c6792f85061107a6b6a957668668fc3c
Roman Lytvynenko <litvinenko95@gmail.com>
2022-09-19 22:09:59 +0300

    added multiple responses support

--- tests/ResponseDocumentingTest.php
+++ tests/ResponseDocumentingTest.php
@@ -37,3 +37,30 @@ class Foo_TestTwo
         ], 500);
     }
 }
+
+test('multiple responses support', function () {
+    \Illuminate\Support\Facades\Route::get('test', [Foo_TestThree::class, 'index']);
+
+    \Dedoc\Scramble\Scramble::routes(fn (Route $r) => $r->uri === 'test');
+    $openApiDocument = app()->make(\Dedoc\Scramble\Generator::class)();
+
+    assertMatchesSnapshot($openApiDocument);
+});
+class Foo_TestThree
+{
+    public function index()
+    {
+        try {
+            something_some();
+        } catch (\Throwable $e) {
+            return response()->json([
+                /** @var array{msg: string, code: int} */
+                'error' => $var,
+            ], 500);
+        }
+        if ($foo) {
+            return response()->json(['foo' => 'one']);
+        }
+        return response()->json(['foo' => 'bar']);
+    }
+}
