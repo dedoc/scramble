338a0e679f35e8835db9c97878f6f77fc5be7d5b
romalytvynenko <romalytvynenko@users.noreply.github.com>
2022-09-20 20:42:44 +0000

    Fix styling

--- src/Support/Generator/SecuritySchemes/OAuthFlow.php
+++ src/Support/Generator/SecuritySchemes/OAuthFlow.php
@@ -5,26 +5,32 @@ namespace Dedoc\Scramble\Support\Generator\SecuritySchemes;
 class OAuthFlow
 {
     public string $authorizationUrl = '';
+
     public string $tokenUrl = '';
+
     public string $refreshUrl = '';
+
     /** @var array<string, string> */
     public array $scopes = [];
 
     public function authorizationUrl(string $authorizationUrl): OAuthFlow
     {
         $this->authorizationUrl = $authorizationUrl;
+
         return $this;
     }
 
     public function tokenUrl(string $tokenUrl): OAuthFlow
     {
         $this->tokenUrl = $tokenUrl;
+
         return $this;
     }
 
     public function refreshUrl(string $refreshUrl): OAuthFlow
     {
         $this->refreshUrl = $refreshUrl;
+
         return $this;
     }
 
