9980efb964826daef20745fdfbc032444c7f2344
Roman Lytvynenko <litvinenko95@gmail.com>
2022-10-16 13:03:42 +0300

    removed unused type import

--- src/Infer/SimpleTypeGetters/ConstFetchTypeGetter.php
+++ src/Infer/SimpleTypeGetters/ConstFetchTypeGetter.php
@@ -6,7 +6,6 @@ use Dedoc\Scramble\Support\Type\Literal\LiteralBooleanType;
 use Dedoc\Scramble\Support\Type\Type;
 use Dedoc\Scramble\Support\Type\UnknownType;
 use PhpParser\Node;
-use PhpParser\Node\Expr\BooleanNot;
 
 class ConstFetchTypeGetter
 {
