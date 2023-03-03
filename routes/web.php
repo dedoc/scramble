d213aa41b021746bddedea804088afbc2bef2fa1
dmason30 <danmason@spendology.com>
2023-03-03 01:29:56 +0000

    Fix using closure on route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
+use Dedoc\Scramble\Generator;
 use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(config('scramble.middleware', [RestrictedDocsAccess::class]))->group(function () {
-    Route::get('docs/api.json', function (Dedoc\Scramble\Generator $generator) {
-        return $generator();
-    })->name('scramble.docs.index');
+    Route::get('docs/api.json', Generator::class)->name('scramble.docs.index');
 
     Route::view('docs/api', 'scramble::docs')->name('scramble.docs.api');
 });
