c20cca97151771ef8bf07cb8834fb4d2e14ee570
Roman Lytvynenko <litvinenko95@gmail.com>
2023-06-01 23:29:23 +0300

    small fix

--- src/Infer/Services/FileNameResolver.php
+++ src/Infer/Services/FileNameResolver.php
@@ -18,6 +18,6 @@ class FileNameResolver
     {
         $name = $this->nameContext->getResolvedName(new Name([$shortName]), 1)->toString();
 
-        return (class_exists($name) || interface_exists($name)) ? $name : $shortName;
+        return class_exists($name) || interface_exists($name) ? $name : $shortName;
     }
 }
