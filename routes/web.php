eb2ed01a062741300183891fb67b2a6dab2e28cd
Ben Sherred <ben@sherred.co.uk>
2023-07-30 20:34:09 +0100

    feat: apply path to docs JSON route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(config('scramble.middleware', [RestrictedDocsAccess::class]))->group(function () {
-    Route::get('docs/api.json', function (Dedoc\Scramble\Generator $generator) {
+    Route::get(config('scramble.path', 'docs/api') . '.json', function (Dedoc\Scramble\Generator $generator) {
         return $generator();
     })->name('scramble.docs.index');
 
