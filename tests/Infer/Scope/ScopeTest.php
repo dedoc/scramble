b3bcbc3f51639bd675f06980da3c9403ca550637
romalytvynenko <litvinenko95@gmail.com>
2023-08-06 20:12:32 +0300

    fixed scopes getType returning null (#167)

---
+++ tests/Infer/Scope/ScopeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+function getStatementTypeForScopeTest(string $statement, array $extensions = [])
+{
+    return analyzeFile('<?php', $extensions)->getExpressionType($statement);
+}
+
+it('infers property fetch nodes types', function ($code, $expectedTypeString) {
+    expect(getStatementTypeForScopeTest($code)->toString())->toBe($expectedTypeString);
+})->with([
+    ['$foo->bar', 'unknown'],
+    ['$foo->bar->{"baz"}', 'unknown'],
+]);
