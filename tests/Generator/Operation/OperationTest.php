0f6a798d33863fa695c3a824c98c9de0c3766134
Bruno Drzanic <bruno@simple-code.hr>
2024-01-08 22:49:29 +0100

    test: extend operation test

--- tests/Generator/Operation/OperationTest.php
+++ tests/Generator/Operation/OperationTest.php
@@ -10,3 +10,12 @@ it('deprecated key is properly set', function () {
         ->and($array)->toHaveKey('deprecated')
         ->and($array['deprecated'])->toBeTrue();
 });
+
+it('default deprecated key is false', function () {
+    $operation = new \Dedoc\Scramble\Support\Generator\Operation('get');
+
+    $array = $operation->toArray();
+
+    expect($operation->deprecated)->toBeFalse()
+        ->and($array)->not()->toHaveKey('deprecated');
+});
