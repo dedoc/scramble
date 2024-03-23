a7378f0e40eba65f8070038af54debaec460e07f
Roman Lytvynenko <litvinenko95@gmail.com>
2024-03-23 22:15:18 +0200

    fixed tests

--- tests/Support/OperationExtensions/RequestBodyExtensionTest.php
+++ tests/Support/OperationExtensions/RequestBodyExtensionTest.php
@@ -19,109 +19,24 @@ class RequestBodyExtensionTest__uses_application_json_as_default
     }
 }
 
-it('does generate for delete by default', function () {
-    $openApiDocument = generateForRoute(function () {
-        return RouteFacade::delete('api/test', [RequestBodyExtensionTest__does_generate_by_default_delete::class, 'index']);
+it('generates request body only for certain http methods', function (string $method, bool $isRequestBodyExpected) {
+    $openApiDocument = generateForRoute(function () use ($method) {
+        return RouteFacade::addRoute($method, 'api/test', [RequestBodyExtensionTest__generates_request_body_only_for_certain_http_methods::class, 'index']);
     });
 
-    expect($openApiDocument['paths']['/test']['delete'])
-        ->toHaveKey('requestBody')
-        ->and($openApiDocument['paths']['/test']['delete'])
-        ->not()->toHaveKey('parameters')
-        ->and($openApiDocument['paths']['/test']['delete']['requestBody']['content'])
-        ->toHaveKey('application/json')
-        ->toHaveLength(1)
-        ->and(
-            data_get(
-                $openApiDocument,
-                'paths./test.delete.requestBody.content.application/json.schema.properties'
-            )
-        )
-        ->toHaveKey('foo')
-        ->toHaveKey('foo.type', 'string');
-
-});
-class RequestBodyExtensionTest__does_generate_by_default_delete
-{
-    public function index(Illuminate\Http\Request $request)
-    {
-        $request->validate(['foo' => 'string']);
-    }
-}
-
-it('does generate for head by default', function () {
-    $openApiDocument = generateForRoute(function () {
-        return RouteFacade::addRoute('head', 'api/test', [RequestBodyExtensionTest__does_generate_by_default_head::class, 'index']);
-    });
-
-    expect($openApiDocument['paths']['/test']['head'])
-        ->toHaveKey('requestBody')
-        ->and($openApiDocument['paths']['/test']['head'])
-        ->not()->toHaveKey('parameters')
-        ->and($openApiDocument['paths']['/test']['head']['requestBody']['content'])
-        ->toHaveKey('application/json')
-        ->toHaveLength(1)
-        ->and(
-            data_get(
-                $openApiDocument,
-                'paths./test.head.requestBody.content.application/json.schema.properties'
-            )
-        )
-        ->toHaveKey('foo')
-        ->toHaveKey('foo.type', 'string');
-
-});
-class RequestBodyExtensionTest__does_generate_by_default_head
-{
-    public function index(Illuminate\Http\Request $request)
-    {
-        $request->validate(['foo' => 'string']);
-    }
-}
-
-it('does not generate for get by default', function () {
-    $openApiDocument = generateForRoute(function () {
-        return RouteFacade::get('api/test', [RequestBodyExtensionTest__does_not_generate_by_default_get::class, 'index']);
-    });
-
-    expect($openApiDocument['paths']['/test']['get'])
-        ->not()->toHaveKey('requestBody')
-        ->and($openApiDocument['paths']['/test']['get'])
-        ->toHaveKey('parameters')
-        ->and($openApiDocument['paths']['/test']['get']['parameters'])
-        ->toHaveLength(1)
-        ->and($openApiDocument['paths']['/test']['get']['parameters']['0'])
-        ->toHaveKey('name', 'foo')
-        ->toHaveKey('in', 'query');
-
-});
-class RequestBodyExtensionTest__does_not_generate_by_default_get
-{
-    public function index(Illuminate\Http\Request $request)
-    {
-        $request->validate(['foo' => 'string']);
-    }
-}
-
-it('does not generate for delete when configured', function () {
-    config()->set('scramble.disallow_request_body', ['get', 'delete']);
-
-    $openApiDocument = generateForRoute(function () {
-        return RouteFacade::delete('api/test', [RequestBodyExtensionTest__does_not_generate_when_configured_delete::class, 'index']);
-    });
-
-    expect($openApiDocument['paths']['/test']['delete'])
-        ->not()->toHaveKey('requestBody')
-        ->and($openApiDocument['paths']['/test']['delete'])
-        ->toHaveKey('parameters')
-        ->and($openApiDocument['paths']['/test']['delete']['parameters'])
-        ->toHaveLength(1)
-        ->and($openApiDocument['paths']['/test']['delete']['parameters']['0'])
-        ->toHaveKey('name', 'foo')
-        ->toHaveKey('in', 'query');
-
-});
-class RequestBodyExtensionTest__does_not_generate_when_configured_delete
+    expect($openApiDocument['paths']['/test'][$method])
+        ->toHaveKeys($isRequestBodyExpected ? ['requestBody'] : ['parameters'])
+        ->and($openApiDocument['paths']['/test'][$method])
+        ->not()->toHaveKeys($isRequestBodyExpected ? ['parameters'] : ['requestBody']);
+})->with([
+    ['post', true],
+    ['put', true],
+    ['patch', true],
+    ['get', false],
+    ['head', false],
+    ['delete', false],
+]);
+class RequestBodyExtensionTest__generates_request_body_only_for_certain_http_methods
 {
     public function index(Illuminate\Http\Request $request)
     {
