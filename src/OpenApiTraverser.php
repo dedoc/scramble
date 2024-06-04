9db368ac22d8bb877ccc41609d462cd1954be877
romalytvynenko <litvinenko95@gmail.com>
2024-06-04 16:49:40 +0300

    Added OpenAPI document traverser (#406)
    * added traverser
    
    * added an api to traverse an openapi document
    
    * Fix styling
    
    * simplified class names
    
    * rename classes to match naming
    
    * more sensible defaults so data_get can work based on given path
    
    ---------
    
    Co-authored-by: romalytvynenko <romalytvynenko@users.noreply.github.com>

---
+++ src/OpenApiTraverser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dedoc\Scramble;
+
+class OpenApiTraverser
+{
+    public function __construct(
+        private array $visitors = [],
+    ) {
+    }
+
+    public function traverse($type, $path = []): void
+    {
+        if (is_scalar($type) || $type === null) {
+            return;
+        }
+
+        $this->enterType($type, $path);
+
+        $propertiesWithNodes = $this->getNodes($type);
+
+        foreach ($propertiesWithNodes as $propertyWithNode) {
+            $node = $type->$propertyWithNode;
+
+            if (! is_array($node)) {
+                $this->traverse($node, [...$path, $propertyWithNode]);
+            } else {
+                foreach ($node as $i => $item) {
+                    $this->traverse($item, [...$path, $propertyWithNode, $i]);
+                }
+            }
+        }
+
+        $this->leaveType($type, $path);
+    }
+
+    private function enterType($type, $path): void
+    {
+        foreach ($this->visitors as $visitor) {
+            $visitor->enter($type, $path);
+        }
+    }
+
+    private function leaveType($type, $path): void
+    {
+        foreach ($this->visitors as $visitor) {
+            $visitor->leave($type, $path);
+        }
+    }
+
+    private function getNodes($instance)
+    {
+        return array_keys(get_object_vars($instance));
+    }
+}
