4c14788da938043e1f01cbd331ecfc2efdf692e2
Vladimir <kotopes231@gmail.com>
2024-09-14 18:02:39 +0300

    reduce duplication in the generated output file by adding and using PaginatedSetMeta and PaginatedSetLinks schemas

--- src/Support/TypeToSchemaExtensions/LengthAwarePaginatorTypeToSchema.php
+++ src/Support/TypeToSchemaExtensions/LengthAwarePaginatorTypeToSchema.php
@@ -3,6 +3,7 @@
 namespace Dedoc\Scramble\Support\TypeToSchemaExtensions;
 
 use Dedoc\Scramble\Extensions\TypeToSchemaExtension;
+use Dedoc\Scramble\Support\Generator\Reference;
 use Dedoc\Scramble\Support\Generator\Response;
 use Dedoc\Scramble\Support\Generator\Schema;
 use Dedoc\Scramble\Support\Generator\Types\ArrayType;
@@ -43,34 +44,53 @@ class LengthAwarePaginatorTypeToSchema extends TypeToSchemaExtension
         }
 
         $type = new OpenApiObjectType;
-        $type->addProperty('data', (new ArrayType)->setItems($collectingType));
+        $type->addProperty('data', (new ArrayType())->setItems($collectingType));
+
+
+        $links = (new OpenApiObjectType)
+            ->addProperty('first', (new StringType)->nullable(true))
+            ->addProperty('last', (new StringType)->nullable(true))
+            ->addProperty('prev', (new StringType)->nullable(true))
+            ->addProperty('next', (new StringType)->nullable(true))
+            ->setRequired(['first', 'last', 'prev', 'next']);
+
+        $linksSchema = Schema::fromType($links);
+        $linksSchemaName = 'PaginatedSetLinks';
+        if(!$this->components->hasSchema($linksSchemaName)) {
+            $this->components->addSchema($linksSchemaName, $linksSchema);
+        }
+
         $type->addProperty(
             'links',
-            (new OpenApiObjectType)
-                ->addProperty('first', (new StringType)->nullable(true))
-                ->addProperty('last', (new StringType)->nullable(true))
-                ->addProperty('prev', (new StringType)->nullable(true))
-                ->addProperty('next', (new StringType)->nullable(true))
-                ->setRequired(['first', 'last', 'prev', 'next'])
+            new Reference('schemas', $linksSchemaName, $this->components)
         );
+
+        $meta = (new OpenApiObjectType)
+            ->addProperty('current_page', new IntegerType)
+            ->addProperty('from', (new IntegerType)->nullable(true))
+            ->addProperty('last_page', new IntegerType)
+            ->addProperty('links', (new ArrayType)->setItems(
+                (new OpenApiObjectType)
+                    ->addProperty('url', (new StringType)->nullable(true))
+                    ->addProperty('label', new StringType)
+                    ->addProperty('active', new BooleanType)
+                    ->setRequired(['url', 'label', 'active'])
+            )->setDescription('Generated paginator links.'))
+            ->addProperty('path', (new StringType)->nullable(true)->setDescription('Base path for paginator generated URLs.'))
+            ->addProperty('per_page', (new IntegerType)->setDescription('Number of items shown per page.'))
+            ->addProperty('to', (new IntegerType)->nullable(true)->setDescription('Number of the last item in the slice.'))
+            ->addProperty('total', (new IntegerType)->setDescription('Total number of items being paginated.'))
+            ->setRequired(['current_page', 'from', 'last_page', 'links', 'path', 'per_page', 'to', 'total']);
+
+        $metaSchema = Schema::fromType($meta);
+        $metaSchemaName = 'PaginatedSetMeta';
+        if(!$this->components->hasSchema($metaSchemaName)) {
+            $this->components->addSchema($metaSchemaName, $metaSchema);
+        }
+
         $type->addProperty(
             'meta',
-            (new OpenApiObjectType)
-                ->addProperty('current_page', new IntegerType)
-                ->addProperty('from', (new IntegerType)->nullable(true))
-                ->addProperty('last_page', new IntegerType)
-                ->addProperty('links', (new ArrayType)->setItems(
-                    (new OpenApiObjectType)
-                        ->addProperty('url', (new StringType)->nullable(true))
-                        ->addProperty('label', new StringType)
-                        ->addProperty('active', new BooleanType)
-                        ->setRequired(['url', 'label', 'active'])
-                )->setDescription('Generated paginator links.'))
-                ->addProperty('path', (new StringType)->nullable(true)->setDescription('Base path for paginator generated URLs.'))
-                ->addProperty('per_page', (new IntegerType)->setDescription('Number of items shown per page.'))
-                ->addProperty('to', (new IntegerType)->nullable(true)->setDescription('Number of the last item in the slice.'))
-                ->addProperty('total', (new IntegerType)->setDescription('Total number of items being paginated.'))
-                ->setRequired(['current_page', 'from', 'last_page', 'links', 'path', 'per_page', 'to', 'total'])
+            new Reference('schemas', $metaSchemaName, $this->components)
         );
         $type->setRequired(['data', 'links', 'meta']);
 
