921aef7aa4ac22490e137f8bca8b8ff90fcaa39b
romalytvynenko <romalytvynenko@users.noreply.github.com>
2024-10-08 18:58:01 +0000

    Fix styling

--- src/Extensions/TypeToSchemaExtension.php
+++ src/Extensions/TypeToSchemaExtension.php
@@ -5,10 +5,10 @@ namespace Dedoc\Scramble\Extensions;
 use Dedoc\Scramble\Infer;
 use Dedoc\Scramble\Support\Generator\Components;
 use Dedoc\Scramble\Support\Generator\Response;
+use Dedoc\Scramble\Support\Generator\Types\Type as OpenApiType;
 use Dedoc\Scramble\Support\Generator\Types\UnknownType;
 use Dedoc\Scramble\Support\Generator\TypeTransformer;
 use Dedoc\Scramble\Support\Type\Type;
-use Dedoc\Scramble\Support\Generator\Types\Type as OpenApiType;
 
 abstract class TypeToSchemaExtension
 {
@@ -26,8 +26,8 @@ abstract class TypeToSchemaExtension
     }
 
     /**
-     * @param Type $type The type being transformed to schema.
-     * @param ?OpenApiType $previousExtensionResult The resulting schema from a previous extension.
+     * @param  Type  $type  The type being transformed to schema.
+     * @param  ?OpenApiType  $previousExtensionResult  The resulting schema from a previous extension.
      */
     public function toSchema(Type $type)
     {
@@ -35,8 +35,8 @@ abstract class TypeToSchemaExtension
     }
 
     /**
-     * @param Type $type The type being transformed to response.
-     * @param ?Response $previousExtensionResult The resulting response from a previous extension.
+     * @param  Type  $type  The type being transformed to response.
+     * @param  ?Response  $previousExtensionResult  The resulting response from a previous extension.
      */
     public function toResponse(Type $type)
     {
