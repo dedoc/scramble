2fc38203540a4ab2f6d67ce2ab07e26d511e7bdf
Roman Lytvynenko <litvinenko95@gmail.com>
2024-10-17 10:43:35 +0300

    marked method calls

--- src/Support/OperationExtensions/RulesExtractor/RequestMethodCallsExtractor.php
+++ src/Support/OperationExtensions/RulesExtractor/RequestMethodCallsExtractor.php
@@ -2,6 +2,7 @@
 
 namespace Dedoc\Scramble\Support\OperationExtensions\RulesExtractor;
 
+use Dedoc\Scramble\Support\Generator\Parameter;
 use Dedoc\Scramble\Support\RouteInfo;
 
 class RequestMethodCallsExtractor implements RulesExtractor
@@ -14,7 +15,13 @@ class RequestMethodCallsExtractor implements RulesExtractor
     public function extract(RouteInfo $routeInfo): ParametersExtractionResult
     {
         return new ParametersExtractionResult(
-            parameters: array_values($routeInfo->requestParametersFromCalls->data),
+            parameters: array_map(
+                function (Parameter $p) {
+                    $p->setAttribute('isFromMethodCall', true);
+                    return $p;
+                },
+                array_values($routeInfo->requestParametersFromCalls->data),
+            ),
         );
     }
 }
