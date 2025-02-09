88314d22545b1fcd1f18aaec8ad90ea3ab60a80b
romalytvynenko <litvinenko95@gmail.com>
2025-02-09 18:04:19 +0200

    `security` implementation according OpenAPI 3.1.0 specification (#717)
    * make security implementation follow the specification
    
    * make security the same for openapi and operation objects
    
    * Fix styling
    
    * handle missing security in secure  method
    
    ---------
    
    Co-authored-by: romalytvynenko <romalytvynenko@users.noreply.github.com>

--- tests/OpenApiBuildersTest.php
+++ tests/OpenApiBuildersTest.php
@@ -2,6 +2,7 @@
 
 use Dedoc\Scramble\Support\Generator\InfoObject;
 use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Dedoc\Scramble\Support\Generator\SecuritySchemes\OAuthFlow;
 
@@ -57,4 +58,36 @@ it('builds oauth2 security scheme with empty scope map', function () {
 
     expect($document['components']['securitySchemes']['oauth2']['flows']['implicit']['scopes'])
         ->toBeObject();
+});
+
+it('allows securing with complex security rules', function () {
+    $openApi = (new OpenApi('3.1.0'))
+        ->setInfo(InfoObject::make('API')->setVersion('0.0.1'));
+
+    $openApi->components->securitySchemes['tenant'] = SecurityScheme::apiKey('header', 'X-Tenant');
+    $openApi->components->securitySchemes['bearer'] = SecurityScheme::http('bearer');
+
+    $openApi->security[] = new SecurityRequirement([
+        'tenant' => [],
+        'bearer' => [],
+    ]);
+
+    $serialized = $openApi->toArray();
+
+    expect($serialized['security'])->toBe([[
+        'tenant' => [],
+        'bearer' => [],
+    ]])
+        ->and($serialized['components']['securitySchemes'])
+        ->toBe([
+            'tenant' => [
+                'type' => 'apiKey',
+                'in' => 'header',
+                'name' => 'X-Tenant',
+            ],
+            'bearer' => [
+                'type' => 'http',
+                'scheme' => 'bearer',
+            ],
+        ]);
 });
