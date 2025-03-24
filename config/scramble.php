d9041eea6af67a3166ee3bf84a14e809c6d3c985
superdejooo <superdejooo@gmail.com>
2025-03-24 11:50:41 +0100

    feat: add configurable layout option for Spotlight UI
    - Introduce new config key `scramble.ui.layout` to allow users to select
      from different Spotlight layouts supported by Stoplight Elements.
    - Pass the layout configuration to the Spotlight component during initialization.
    - Default to the sidebar layout if no custom layout is provided.

--- config/scramble.php
+++ config/scramble.php
@@ -65,6 +65,14 @@ return [
          * Use to fetch the credential policy for the Try It feature. Options are: omit, include (default), and same-origin
          */
         'try_it_credentials_policy' => 'include',
+
+		/*
+		 * There are three layouts for Elements:
+		 * - sidebar - (default) Three-column design with a sidebar that can be resized.
+		 * - responsive - Like sidebar, except at small screen sizes it collapses the sidebar into a drawer that can be toggled open.
+		 * - stacked - Everything in a single column, making integrations with existing websites that have their own sidebar or other columns already.
+		 */
+		'layout' => 'sidebar',
     ],
 
     /*
