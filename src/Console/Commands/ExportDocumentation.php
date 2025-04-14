adb48db3b8d77e2500744a885b3852ac3eb2f950
Lucas Bittencourt <lucascbittencourt1@gmail.com>
2025-04-14 10:33:40 -0300

    Fixed ExportDocumentation command (#800)
    * fix(export): updated filename logic to consider the api option
    
    * fix(export): added json flag to not scape slashes
    
    * test(export): added tests for the export command

--- src/Console/Commands/ExportDocumentation.php
+++ src/Console/Commands/ExportDocumentation.php
@@ -18,12 +18,15 @@ class ExportDocumentation extends Command
 
     public function handle(Generator $generator): void
     {
-        $config = Scramble::getGeneratorConfig($api = $this->option('api'));
+        $api = $this->option('api');
+        $path = $this->option('path');
 
-        $specification = json_encode($generator($config), JSON_PRETTY_PRINT);
+        $config = Scramble::getGeneratorConfig($api);
+
+        $specification = json_encode($generator($config), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
         /** @var string $filename */
-        $filename = $this->option('path') ?? $config->get('export_path', 'api'.($api === 'default' ? '' : "-$api").'.json');
+        $filename = $path ?: $config->get('export_path') ?? 'api'.($api === 'default' ? '' : "-$api").'.json';
 
         File::put($filename, $specification);
 
