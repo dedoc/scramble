cb8c227e8fd83ef6219acfdf2bdad129b4b1cbd7
Roman Lytvynenko <litvinenko95@gmail.com>
2025-07-08 13:31:35 +0300

    added a test

--- tests/Attributes/ResponseTest.php
+++ tests/Attributes/ResponseTest.php
@@ -19,12 +19,43 @@ it('generates response for basic case', function () {
             'required' => ['foo'],
         ]);
 });
-
 class AController_ResponseTest
 {
     #[Response(200, 'Nice response', type: 'array{"foo": string}')]
     public function __invoke()
     {
+        return something_unknown();
+    }
+}
+
+it('allows adding additional media types response without overriding the original type', function () {
+    $openApiDocument = generateForRoute(fn () => Route::get('test', MultipleTypeController_ResponseTest::class));
+
+    expect($responses = $openApiDocument['paths']['/test']['get']['responses'])
+        ->toHaveCount(1)
+        ->and($responses[200]['description'])
+        ->toBe('When `download` set to `false`, returns the JSON response; when `true`, returns the excel')
+        ->and($responses[200]['content']['application/json']['schema'])
+        ->toBe([
+            'type' => 'object',
+            'properties' => ['foo' => ['type' => 'string']],
+            'required' => ['foo'],
+        ])
+        ->and($responses[200]['content']['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']['schema'])
+        ->toBe([
+            'type' => 'string',
+            'format' => 'binary',
+        ]);
+});
+class MultipleTypeController_ResponseTest
+{
+    #[Response(200, 'When `download` set to `false`, returns the JSON response; when `true`, returns the excel')]
+    #[Response(200, mediaType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', type: 'string', format: 'binary')]
+    public function __invoke()
+    {
+        /**
+         * @body array{"foo":string}
+         */
         return something_unknown();
     }
 }
