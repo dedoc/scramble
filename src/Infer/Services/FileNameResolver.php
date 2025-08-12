88d045dfeb1c71871d1c476fdad073c10ad70b8d
romalytvynenko <litvinenko95@gmail.com>
2025-08-12 13:17:49 +0300

    Fixed duplicated schemas in generated spec when using FQN with leading slash in PHPDoc (#929)
    * fqn is returned from FileNameResolver
    
    * added fqn comment
    
    * fix typo

--- src/Infer/Services/FileNameResolver.php
+++ src/Infer/Services/FileNameResolver.php
@@ -55,6 +55,8 @@ class FileNameResolver
     {
         $name = $this->nameContext->getResolvedName(new Name([$shortName]), 1)->toString();
 
-        return class_exists($name) || interface_exists($name) ? $name : $shortName;
+        // By definition, the returned class name here is FQN, so like *::class or get_class(*)
+        // invoking name resolver returns the class name without leading slash.
+        return ltrim(class_exists($name) || interface_exists($name) ? $name : $shortName, '\\');
     }
 }
