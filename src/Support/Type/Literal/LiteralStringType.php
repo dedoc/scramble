7af06fe46b4242d97d8815f570e1e29627e774b7
Roman Lytvynenko <litvinenko95@gmail.com>
2025-09-08 12:48:36 +0300

    common contract for literl string

--- src/Support/Type/Literal/LiteralStringType.php
+++ src/Support/Type/Literal/LiteralStringType.php
@@ -2,15 +2,21 @@
 
 namespace Dedoc\Scramble\Support\Type\Literal;
 
+use Dedoc\Scramble\Support\Type\Contracts\LiteralString;
 use Dedoc\Scramble\Support\Type\StringType;
 
-class LiteralStringType extends StringType
+class LiteralStringType extends StringType implements LiteralString
 {
     public string $value;
 
     public function __construct(string $value)
     {
         $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
     }
 
     public function toString(): string
