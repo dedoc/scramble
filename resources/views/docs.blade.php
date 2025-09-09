fdedbaac3271b466182d4029651aa90eac53cee5
romalytvynenko <litvinenko95@gmail.com>
2025-09-09 17:36:44 +0300

    fix for elements dark mode (#953)

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,8 +6,8 @@
     <meta name="color-scheme" content="{{ $config->get('ui.theme', 'light') }}">
     <title>{{ $config->get('ui.title', config('app.name') . ' - API Docs') }}</title>
 
-    <script src="https://unpkg.com/@stoplight/elements@8.3.4/web-components.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@8.3.4/styles.min.css">
+    <script src="https://unpkg.com/@stoplight/elements@9.0.6/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@9.0.6/styles.min.css">
 
     <script>
         const originalFetch = window.fetch;
@@ -48,6 +48,32 @@
             return originalFetch(url, options);
         };
     </script>
+
+    <style>
+        html, body { margin:0; height:100%; }
+        body { background-color: var(--color-canvas); }
+        /* issues about the dark theme of stoplight/mosaic-code-viewer using web component:
+         * https://github.com/stoplightio/elements/issues/2188#issuecomment-1485461965
+         */
+        [data-theme="dark"] .token.property {
+            color: rgb(128, 203, 196) !important;
+        }
+        [data-theme="dark"] .token.operator {
+            color: rgb(255, 123, 114) !important;
+        }
+        [data-theme="dark"] .token.number {
+            color: rgb(247, 140, 108) !important;
+        }
+        [data-theme="dark"] .token.string {
+            color: rgb(165, 214, 255) !important;
+        }
+        [data-theme="dark"] .token.boolean {
+            color: rgb(121, 192, 255) !important;
+        }
+        [data-theme="dark"] .token.punctuation {
+            color: #dbdbdb !important;
+        }
+    </style>
 </head>
 <body style="height: 100vh; overflow-y: hidden">
 <elements-api
