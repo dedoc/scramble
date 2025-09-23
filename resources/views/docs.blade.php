e65d7f47401bf38ee98738a72826d70aa86f6863
romalytvynenko <litvinenko95@gmail.com>
2025-09-23 10:11:25 +0300

    lock elements on 8.4.2 (#964)

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,8 +6,8 @@
     <meta name="color-scheme" content="{{ $config->get('ui.theme', 'light') }}">
     <title>{{ $config->get('ui.title', config('app.name') . ' - API Docs') }}</title>
 
-    <script src="https://unpkg.com/@stoplight/elements@9.0.6/web-components.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@9.0.6/styles.min.css">
+    <script src="https://unpkg.com/@stoplight/elements@8.4.2/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@8.4.2/styles.min.css">
 
     <script>
         const originalFetch = window.fetch;
