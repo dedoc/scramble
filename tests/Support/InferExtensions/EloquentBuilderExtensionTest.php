29a1a8168eabd54a8dd206af38ce162739023f29
Roman Lytvynenko <litvinenko95@gmail.com>
2025-10-06 15:54:10 +0300

    fix tests

--- tests/Support/InferExtensions/EloquentBuilderExtensionTest.php
+++ tests/Support/InferExtensions/EloquentBuilderExtensionTest.php
@@ -6,6 +6,8 @@ use Dedoc\Scramble\Tests\Files\SamplePostModel;
 use Illuminate\Database\Eloquent\Builder;
 
 test('forwards call to scope', function (string $method, string $expectedType) {
+    dump(getStatementType(SamplePostModel::class.'::query()'));
+
     $type = getStatementType(SamplePostModel::class.'::query()->'.$method.'()');
 
     expect($type->toString())->toBe($expectedType);
