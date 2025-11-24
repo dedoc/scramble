e829ea6e5c6afd0f133989ae4a0e132e0572c52b
Roman Lytvynenko <litvinenko95@gmail.com>
2025-11-24 11:25:33 +0200

    prevent documenting non-instantiable classes

--- src/Support/TypeToSchemaExtensions/ArrayableToSchema.php
+++ src/Support/TypeToSchemaExtensions/ArrayableToSchema.php
@@ -32,7 +32,8 @@ class ArrayableToSchema extends TypeToSchemaExtension
     {
         return $type instanceof ObjectType
             && $type->isInstanceOf(Arrayable::class)
-            && ! $type->isInstanceOf(Collection::class); // handled in its own extension
+            && ! $type->isInstanceOf(Collection::class) // handled in its own extension
+            && ((new \ReflectionClass($type->name))->isInstantiable());
     }
 
     /**
