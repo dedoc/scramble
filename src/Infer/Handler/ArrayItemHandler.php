9dc1643e9245d04efbe38f6e533eb735d828789d
Roman Lytvynenko <litvinenko95@gmail.com>
2025-12-28 14:44:16 +0200

    evaluating array keys when keys are const fetch expressions

--- src/Infer/Handler/ArrayItemHandler.php
+++ src/Infer/Handler/ArrayItemHandler.php
@@ -3,8 +3,10 @@
 namespace Dedoc\Scramble\Infer\Handler;
 
 use Dedoc\Scramble\Infer\Scope\Scope;
+use Dedoc\Scramble\Support\OperationExtensions\RulesEvaluator\ConstFetchEvaluator;
 use Dedoc\Scramble\Support\Type\ArrayItemType_;
-use Dedoc\Scramble\Support\Type\Literal\LiteralStringType;
+use PhpParser\ConstExprEvaluationException;
+use PhpParser\ConstExprEvaluator;
 use PhpParser\Node;
 
 class ArrayItemHandler
@@ -21,12 +23,38 @@ class ArrayItemHandler
         $scope->setType(
             $node,
             new ArrayItemType_(
-                $keyType instanceof LiteralStringType ? $keyType->value : null, // @todo handle cases when key is something dynamic
+                $this->evaluateKeyNode($node->key, $scope), // @todo handle cases when key is something dynamic
                 $scope->getType($node->value),
                 isOptional: false,
                 shouldUnpack: $node->unpack,
                 keyType: $keyType,
             )
         );
+    }
+
+    private function evaluateKeyNode(?Node\Expr $key, Scope $scope): int|string|null
+    {
+        if (! $key) {
+            return null;
+        }
+
+        $evaluator = new ConstExprEvaluator(function (Node\Expr $node) use ($scope) {
+            if ($node instanceof Node\Expr\ClassConstFetch) {
+                return (new ConstFetchEvaluator([
+                    'self' => $scope->classDefinition()?->name,
+                    'static' => $scope->classDefinition()?->name,
+                ]))->evaluate($node);
+            }
+
+            return null;
+        });
+
+        try {
+            $result = $evaluator->evaluateSilently($key);
+
+            return is_string($result) || is_int($result) ? $result : null;
+        } catch (ConstExprEvaluationException) {
+            return null;
+        }
     }
 }
