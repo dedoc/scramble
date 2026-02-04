e5ba30ef3fba06383f1aa226b313a66359c6f890
Roman Lytvynenko <litvinenko95@gmail.com>
2026-02-04 17:44:05 +0200

    wip wip

--- src/Support/Type/OffsetSetType.php
+++ src/Support/Type/OffsetSetType.php
@@ -31,7 +31,7 @@ class OffsetSetType extends AbstractType implements LateResolvingType
         }
 
         if ($this->type instanceof ArrayType) {
-            //            return $this->type; // ??
+//                        return $this->type; // ??
         }
 
         $path = $this->normalizePath($this->offset);
@@ -39,7 +39,10 @@ class OffsetSetType extends AbstractType implements LateResolvingType
             return new UnknownType;
         }
 
-        return $this->applyPath($this->type->clone(), $path, $this->value);
+//        return $this->applyPath_OLD($this->type->clone(), $path, $this->value);
+        $result = $this->type->clone();
+
+        return $this->applyPath($result, $path, $this->value);
     }
 
     public function acceptedBy(Type $otherType): bool
@@ -67,10 +70,85 @@ class OffsetSetType extends AbstractType implements LateResolvingType
         return 'OffsetSet<'.$this->type->toString().', '.$this->offset->toString().', '.$this->value->toString().'>';
     }
 
+    public function applyPath(ArrayType|KeyedArrayType &$target, array $path, Type $value): ArrayType|KeyedArrayType
+    {
+        if (count($path) === 0) {
+            return $target;
+        }
+
+        $pathItem = array_shift($path);
+
+        if ($pathItem === null && $target instanceof KeyedArrayType && count($target->items) === 0) {
+            $target = new ArrayType();
+
+            if ($path) {
+                $target->value = new KeyedArrayType();
+
+                $this->applyPath($target->value, $path, $value);
+            } else {
+                $target->value = $value;
+            }
+
+            return $target;
+        }
+
+        if ($target instanceof ArrayType) {
+            $target->value = Union::wrap([$target->value, $value]);
+
+            $this->applyPath($target, $path, $value);
+
+            return $target;
+        }
+
+        $targetItem = Arr::first(
+            $target->items,
+            fn (ArrayItemType_ $t) => $t->key === $pathItem,
+        );
+
+        if ($targetItem) {
+            if ($path) {
+                if (! $targetItem->value instanceof KeyedArrayType && ! $targetItem->value instanceof ArrayType) {
+                    return $target;
+                }
+
+                $this->applyPath($targetItem->value, $path, $value);
+            } else {
+                $targetItem->value = $value;
+            }
+        } else {
+            if ($path) {
+                $targetItem = new ArrayItemType_(
+                    key: $pathItem,
+                    value: $newModifyingType = new KeyedArrayType,
+                );
+                $target->items[] = $targetItem;
+
+                $this->applyPath($newModifyingType, $path, $value);
+            } else {
+                $target->items[] = new ArrayItemType_(key: $pathItem, value: $value);
+            }
+        }
+
+        $target->isList = KeyedArrayType::checkIsList($target->items);
+
+        return $target;
+
+
+        if ($pathItem === null) {
+//            if ($target instanceof KeyedArrayType && count($target->items) === 0) {
+//
+//            }
+        } elseif (is_string($pathItem) || is_int($pathItem)) {
+            dd($pathItem);
+        }
+
+        return $target;
+    }
+
     /**
      * @param  non-empty-list<int, int|string|null>  $path
      */
-    private function applyPath(ArrayType|KeyedArrayType $target, array $path, Type $value): ArrayType|KeyedArrayType
+    private function applyPath_OLD(ArrayType|KeyedArrayType $target, array $path, Type $value): ArrayType|KeyedArrayType
     {
         $modifyingType = $target;
 
@@ -97,7 +175,7 @@ class OffsetSetType extends AbstractType implements LateResolvingType
         );
 
         if ($targetItem) {
-            if (! $targetItem->value instanceof KeyedArrayType) {
+            if (! $targetItem->value instanceof KeyedArrayType && ! $targetItem->value instanceof ArrayType) {
                 return null;
             }
             $newModifyingType = $targetItem->value;
