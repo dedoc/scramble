516f1f2612fe21c26a099d10314ecaadfa59a9f0
Roman Lytvynenko <litvinenko95@gmail.com>
2026-03-17 22:28:28 +0100

    added phpstan self out and improved closure param inference

--- tests/Infer/DefinitionBuilders/FunctionLikeReflectionDefinitionBuilderTest.php
+++ tests/Infer/DefinitionBuilders/FunctionLikeReflectionDefinitionBuilderTest.php
@@ -3,9 +3,36 @@
 namespace Dedoc\Scramble\Tests\Infer\DefinitionBuilders;
 
 use Dedoc\Scramble\Infer\DefinitionBuilders\FunctionLikeReflectionDefinitionBuilder;
+use Dedoc\Scramble\Support\Type\TemplateType;
+use ReflectionMethod;
 
 test('builds the definition for is_null', function () {
     $def = (new FunctionLikeReflectionDefinitionBuilder('is_null'))->build();
 
     expect($def->type->toString())->toBe('(null|mixed): boolean');
 });
+
+test('builds the definition for phpstan-this-out', function () {
+    $def = (new FunctionLikeReflectionDefinitionBuilder(
+        'updateSelfOut',
+        new ReflectionMethod(PhpstanThisOut_FunctionLikeReflectionDefinitionBuilderTest::class, 'updateSelfOut'),
+        collect(['T' => new TemplateType('T')]),
+    ))->build();
+
+    expect($def->getSelfOutType()?->toString())->toBe('self<int>');
+});
+
+/**
+ * @template T
+ */
+class PhpstanThisOut_FunctionLikeReflectionDefinitionBuilderTest
+{
+    /**
+     * @return $this
+     * @phpstan-this-out static<int>
+     */
+    public function updateSelfOut(): self
+    {
+        return $this;
+    }
+}
