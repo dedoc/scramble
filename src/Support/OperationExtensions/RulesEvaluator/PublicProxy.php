3469bcdfe08f3c85f202e37f363c2462933f9f42
Roman Lytvynenko <litvinenko95@gmail.com>
2026-04-27 23:24:55 +0300

    added this support, improve rules evaluation safety

---
+++ src/Support/OperationExtensions/RulesEvaluator/PublicProxy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dedoc\Scramble\Support\OperationExtensions\RulesEvaluator;
+
+/**
+ * @internal
+ * @template T of object
+ */
+class PublicProxy
+{
+    /** @var T */
+    private object $target;
+
+    /**
+     * @param T $target
+     */
+    public function __construct(object $target)
+    {
+        $this->target = $target;
+    }
+
+    /**
+     * @param string $name
+     * @param array<int, mixed> $arguments
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments): mixed
+    {
+        $method = new \ReflectionMethod($this->target, $name);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($this->target, $arguments);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function __get(string $name): mixed
+    {
+        $prop = new \ReflectionProperty($this->target, $name);
+        $prop->setAccessible(true);
+
+        return $prop->getValue($this->target);
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function __set(string $name, $value): void
+    {
+        $prop = new \ReflectionProperty($this->target, $name);
+        $prop->setAccessible(true);
+
+        $prop->setValue($this->target, $value);
+    }
+}
