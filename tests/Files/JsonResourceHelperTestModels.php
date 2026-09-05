7cf945d17b6f2394c25e3bebb2a879fa1f518ba4
romalytvynenko <litvinenko95@gmail.com>
2026-09-05 13:06:28 +0300

    Validate JSON resources against model resource mappings (#1260)
    * Validate JSON resources against model resource mappings
    
    * Improve shallow class analysis and resource model resolution

---
+++ tests/Files/JsonResourceHelperTestModels.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReverseLookupTest_User extends Model
+{
+    protected $table = 'users';
+}
+
+class ReverseLookupTest_Plain extends Model {}
